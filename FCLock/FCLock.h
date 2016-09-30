9cceac87f2017fc55d10d0e55ecebadc022700d0
Martin Walsh <martin.walsh@gmail.com>
2016-09-30 16:35:24 +0100

    Initial Commit

---
+++ FCLock/FCLock.h
@@ -0,0 +1,19 @@
+//
+//  FCLock.h
+//  FCLock
+//
+//  Created by Martin Walsh on 30/09/2016.
+//  Copyright © 2016 Frosty Cube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FCLock.
+FOUNDATION_EXPORT double FCLockVersionNumber;
+
+//! Project version string for FCLock.
+FOUNDATION_EXPORT const unsigned char FCLockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCLock/PublicHeader.h>
+
+
